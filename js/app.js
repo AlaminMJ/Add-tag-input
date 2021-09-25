c819c2761a8cbf813b9087ed7aeeb7ffb1bf7635
Alamin <alamin.mj.3228@gmail.com>
2021-09-25 18:08:21 +0600

    Coding Javascript

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
-console.log("connect with Javascript");
+const ul = document.getElementById("ul");
+const input = document.querySelector("input");
+
+// add tag
+const addTag = (e) => {
+  if (e.code === "Enter") {
+    console.log(e.target.value);
+  }
+  
+};
+
+// add event lesener input tag
+input.addEventListener("keyup", addTag);

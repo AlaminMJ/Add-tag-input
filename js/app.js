adee1b67fde0ed660df4c3a078b87e43da171bf8
Alamin <alamin.mj.3228@gmail.com>
2021-09-25 19:02:00 +0600

    add js code

--- js/app.js
+++ js/app.js
@@ -1,12 +1,38 @@
 const ul = document.getElementById("ul");
 const input = document.querySelector("input");
 
+//createing tag
+
 // add tag
+const tags = [];
+const createTag = () => {
+  ul.querySelectorAll("li").forEach((li) => {
+    li.remove(); //remove all tags
+  });
+  tags.reverse().forEach((tag) => {
+    const liTag = `<li>${tag}<i class="bx bx-x" onClick="remove(this,'${tag}')"></i></li>`; // Create li tag
+    ul.insertAdjacentHTML("afterbegin", liTag); //insert into html
+  });
+};
+function remove(element, tag) {
+  let index = tags.indexOf(tag);
+  tags = [...tags.slice(0, index), ...tags.slice(index + 1)];
+  element.parentElement.remove();
+}
+// add tag function
 const addTag = (e) => {
   if (e.code === "Enter") {
-    console.log(e.target.value);
+    //if click enter button then run the condition
+    const tag = e.target.value.replace(/\s+/g, " "); // remove extra space
+    if (tag.length > 1 && !tags.includes(tag)) {
+      // chack tag lenth gater than 1 letter and not exits in the list of tags
+      tag.split(",").forEach((tag) => {
+        tags.push(tag);
+        createTag(); //creating tag
+      });
+    }
+    e.target.value = "";
   }
-  
 };
 
 // add event lesener input tag
